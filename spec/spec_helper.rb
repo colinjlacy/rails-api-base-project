01a9d15a62555a729666ff994820e6b89f9f9561
colinjlacy <colin@webcake.co>
2015-06-28 17:56:57 -0500

    [api-auth] creates a session controller with a working login POST req that sends back user info, as well as a working test for invalid login, and a broken test for valid login.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,18 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'support/request_helpers'
+require 'devise'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
+	config.include Request::JsonHelpers, :type => :controller
+	config.include Request::HeadersHelpers, :type => :controller
+	config.include Devise::TestHelpers, :type => :controller
+	config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
     # defined using `chain`, e.g.:
